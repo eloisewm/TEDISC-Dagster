5fadc29b20b17b848a865cc42de29717f5dc29b9
Mark Hepburn <Mark.Hepburn@gmail.com>
2026-04-08 15:21:06 +1000

    Add a dummy download-csv workflow

--- workflow.py
+++ workflow.py
@@ -1,8 +1,35 @@
-from dagster import asset, Definitions
+import csv
+from io import StringIO
 
+import httpx
+from dagster import Definitions, asset
+
+
+@asset
+def wfs_inventory() -> str:
+    url = 'https://geoserver.imas.utas.edu.au/geoserver/NESP/ows'
+    # You could just include these in the url, I'm just being pedantic here:
+    params = {
+        'service': 'WFS',
+        'version': '1.0.0',
+        'request': 'GetFeature',
+        'typeName': 'NESP:NESP_MaC_3_21_ORE_prioritySpp_data_inventory',
+        'outputFormat': 'csv',
+    }
+    res = httpx.get(url, params=params)
+    return res.text
+
+
+@asset
+def save_features(wfs_inventory: str) -> int:
+    data_str = StringIO(wfs_inventory)
+    reader = csv.DictReader(data_str)
+    # print(reader.fieldnames)
+    #
+    return len(list(reader))
 
 @asset
 def hello_world():
     return "Hello, Dagster"
 
-defs = Definitions(assets=[hello_world])
+defs = Definitions(assets=[hello_world, wfs_inventory, save_features])
