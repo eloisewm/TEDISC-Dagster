c68392ddf25368e00baacd927806e808a8deaaed
Eloise Wilson-Mayne <eloisewilsonmayne@gmail.com>
2026-05-04 15:20:15 +1000

    Started iNaturalist asset

--- my_dagster_project/defs/assets.py
+++ my_dagster_project/defs/assets.py
@@ -95,53 +95,58 @@ def flagged_data_bla(context: AssetExecutionContext, deduplicated_bla: pd.DataFr
     df = deduplicated_bla
 
     # SURVEY
+    # Important for AA and PA datatypes
     df["FLAG_SURVEY_ID_BLANK"] = df["survey_id"].isna()
-    df["FLAG_SURVEY_ID_DUPLICATE"] = df["survey_id"].duplicated(keep=False) # keep=False marks initial duplicate value also
     df["FLAG_SURVEY_TYPE_BLANK"] = df["survey_type"].isna()
     df["FLAG_SURVEY_TYPE_UNEXPECTED"] = df["survey_type"].notna() & ~df["survey_type"].isin(BLA_SURVEY_TYPES)
 
 
     # PROGRAM
+    # Important for AA and PA datatypes
     df["FLAG_PROGRAM_NAME_BLANK"] = df["program_name"].isna()
     df["FLAG_PROGRAM_NAME_UNEXPECTED"] = df["program_name"].notna() & ~df["program_name"].isin(BLA_PROGRAM_NAMES)
 
     # SPATIAL
-    # Just noting missing values. I considered recording unexpected values, but there's a chance that BirdLife will at some point 
+    # Important for all datatypes 
+    # Noting missing values only. I considered recording unexpected values, but there's a chance that BirdLife will at some point 
     # host non-australian obs, and in that case we'd want to keep everything 
     df["FLAG_LAT_BLANK"] = df["lat"].isna()
     df["FLAG_LON_BLANK"] = df["lon"].isna()
 
     # ACCURACY 
+    # Important for all data types? Or not important at all? To discuss with Keith
     df["FLAG_ACCURACY_BLANK"] = df["accuracy_in_metres"].isna()
     df["FLAG_ACCURACY_ZERO"] = df["accuracy_in_metres"] == 0
     df["FLAG_ACCURACY_NEGATIVE"] = df["accuracy_in_metres"] < 0
     df["FLAG_ACCURACY_EXTREME"] = df["accuracy_in_metres"] > 10000  # TBD
 
     # TAXONOMY
-    # I will primary refer to the scintific names during QAQC, but worth picking up issues with common names also
+    # Important for all datatypes
+    # I will primarily refer to the scientific names during QAQC, but worth picking up issues with common names also
     df["FLAG_COMMON_NAME_BLANK"] = df["common_name"].isna()
     df["FLAG_COMMON_NAME_UNEXPECTED"] = df["common_name"].notna() & ~df["common_name"].isin(BLA_EXPECTED_COMMON_NAMES)
     df["FLAG_SCIENTIFIC_NAME_BLANK"] = df["scientific_name"].isna()
     df["FLAG_SCIENTIFIC_NAME_UNEXPECTED"] = df["scientific_name"].notna() & ~df["scientific_name"].isin(BLA_EXPECTED_SCIENTIFIC_NAMES)
 
     # COUNT 
-    # In a lot of cases the count won't be important or used - but it WILL need to be noted and flagged for Abundance records. 
+    # Important for AA and PA data types (might also be used for PO? Keith doesn't value counts when PO...)
     df["FLAG_INDIVIDUAL_COUNT_BLANK"] = df["individual_count"].isna()
     df["FLAG_INDIVIDUAL_COUNT_ZERO"] = df["individual_count"] == 0
     df["FLAG_INDIVIDUAL_COUNT_NEGATIVE"] = df["individual_count"] < 0
     df["FLAG_INDIVIDUAL_COUNT_EXTREME"] = df["individual_count"] > 1000  # TBD
 
-    # DATE AND TIME
-    # Blank dates will be important to note - blank times won't be as big of an issue but still worth catching
+    # DATE 
+    # Important for all data types
     df["FLAG_DATE_BLANK"] = df["start_date"].isna() 
-    df["FLAG_START_TIME_BLANK"] = df["start_time"].isna()
 
-    date_pattern = r"^\d{4}-\d{2}-\d{2}$"
+    # Check that date follows the pattern dd/mm/yyyy or d/mm/yyyy
+    date_pattern = r'^\d{1,2}/\d{2}/\d{4}$'
     df["FLAG_START_DATE_FORMAT_UNEXPECTED"] = (
         df["start_date"].notna() & 
-        ~df["start_date"].str.match(date_pattern).fillna(False)
+        ~df["start_date"].str.match(date_pattern)
     )
-
+    
+    # Flag if date is in the future
     today = pd.Timestamp(date.today())
     df["FLAG_DATE_IN_FUTURE"] = (
         df["start_date"].notna() & 
@@ -149,15 +154,19 @@ def flagged_data_bla(context: AssetExecutionContext, deduplicated_bla: pd.DataFr
         (pd.to_datetime(df["start_date"], errors="coerce") > today)  # brackets needed
     )
     
-    time_pattern = r"^\d{2}:\d{2}:\d{2}$"
+    # TIME
+    # Important for some survey_types for AA and PA
+    df["FLAG_START_TIME_BLANK"] = df["start_time"].isna()
+
+    # Check that time follows the format HH:MM:SS or H:MM:SS
+    time_pattern = r"^\d{1,2}:\d{2}:\d{2}$"
     df["FLAG_START_TIME_FORMAT_UNEXPECTED"] = (
         df["start_time"].notna() & 
-        ~df["start_time"].str.match(time_pattern).fillna(False)
+        ~df["start_time"].str.match(time_pattern)
     )
 
     # ALL SPECIES RECORDED
-    # A blank/unusual entry here won't always be an issue, as some programs don't ever ask for this field to be entered.
-    # However, in the programs where it is, it will need to be noted if there are missing or an unexpected values. 
+    # Important to flag for when we infer absences
     df["FLAG_ALL_SPECIES_RECORDED_BLANK"] = df["all_species_recorded"].isna()
     df["FLAG_ALL_SPECIES_RECORDED_UNEXPECTED"] = df["all_species_recorded"].notna() & ~df["all_species_recorded"].isin([True, False])
 
@@ -182,6 +191,45 @@ def flagged_data_bla(context: AssetExecutionContext, deduplicated_bla: pd.DataFr
 
 # @asset(group_name = "iNaturalist")
 # def raw_obs_inat(context: AssetExecutionContext) -> pd.DataFrame:
+#     taxon_name = "Calidris ferruginea"
+
+#     count_response = get_observations(
+#         taxon_name=taxon_name,
+#         quality_grade="research",
+#         count_only=True
+#     )
+#     total = count_response["total_results"]
+#     context.log.info(f"Total observations to download: {total}")
+
+#     all_observations = []
+#     page = 1
+#     per_page = 200
+
+#     while True:
+#         response = get_observations(
+#             taxon_name=taxon_name,
+#             quality_grade="research",
+#             per_page=per_page,
+#             page=page,
+#         )
+
+#         results = response["results"]
+
+#         if not results:
+#             break
+
+#         all_observations.extend(results)
+#         context.log.info(f"Downloaded page {page} — {len(all_observations)}/{total} records")
+#         page += 1
+
+#     df = pd.json_normalize(all_observations)
+
+#     context.add_output_metadata({
+#         "row_count": len(df),
+#         "column_count": len(df.columns),
+#         "taxon": taxon_name,
+#         "quality_grade": "research",
+#     })
     
 
 
