accbe950df74dfc5119b03d5e6639ee6de4e8fad
Eloise Wilson-Mayne <eloisewilsonmayne@gmail.com>
2026-05-07 14:51:51 +1000

    Fixed typo in definitions

--- tedisc_dagster/definitions.py
+++ tedisc_dagster/definitions.py
@@ -3,7 +3,7 @@ from dagster import Definitions
 from tedisc_dagster.defs.assets import raw_obs_bla, validated_structure_bla, deduplicated_bla, flagged_data_bla, typed_bla, padded_bla, raw_obs_inat
 from tedisc_dagster.defs.jobs import inventory_job
 from tedisc_dagster.defs.schedules import daily_inventory_schedule
-from mtedisc_dagster.defs.sensors import new_data_sensor
+from tedisc_dagster.defs.sensors import new_data_sensor
 
 # This is the single object Dagster reads when it starts up.
 # Everything you want Dagster to know about must be listed here.
